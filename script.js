61aaea88570326f581562a3c217deca1561f9a55
dayana <dayanatoccoli@hotmail.com>
2023-09-02 22:38:05 -0300

    primercommit

---
+++ script.js
@@ -0,0 +1,28 @@
+let menu = document.querySelector('#menu-icon');
+let navList = document.querySelector('.nav-list');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navList.classList.toggle('open');
+}
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("carousel-slide");
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slideIndex++;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+
+    setTimeout(showSlides, 10000); // Cambia la imagen cada 10 segundos (ajusta el tiempo según tu preferencia)
+}
